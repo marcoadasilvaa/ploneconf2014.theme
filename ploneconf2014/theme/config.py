4d5c9e755ec475db4552bc8a3c5dcb977159a8cf
Marco Da Silva <markmarco16@hotmail.com>
2014-07-19 10:44:51 -0430

    Integracion con Unittest

---
+++ ploneconf2014/theme/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Contains constants used by setuphandler.py
+"""	
+
+PROJECTNAME = 'ploneconf2014.theme'
+
+DEPENDENCIES = [
+    'plone.api',
+	'setuptools',
+    'z3c.jbot',
+    ]
--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(name='ploneconf2014.theme',
       install_requires=[
           'setuptools',
           'z3c.jbot',
+          'plone.api',
           # -*- Extra requirements: -*-
       ],
       extras_require={
